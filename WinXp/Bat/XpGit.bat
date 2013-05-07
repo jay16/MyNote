2d3ff949224140bb4fd61d926a780b0d43c47cd8
jay16 <jay_li@intfocus.com>
2013-05-07 10:49:35 +0800

    2013-05-07_10-49

--- WinXp/Bat/XpGit.bat
+++ WinXp/Bat/XpGit.bat
@@ -1,10 +1,14 @@
 @echo off
 
+for /f "tokens=1,2,3 delims=- " %%A in ('date /t') do set sDate=%%A-%%B-%%C
+set sTime=%time:~0,2%:%time:~3,2% 
+set commit= %sDate%_%time:~0,2%-%time:~3,2%
+
 cd E:\MyWork\MyNote\
 
 C:\WINDOWS\system32\cmd.exe /c ""D:\Program Files\Git\bin\sh.exe" --login -i -c "git init""
 C:\WINDOWS\system32\cmd.exe /c ""D:\Program Files\Git\bin\sh.exe" --login -i -c "git add -A .""
-C:\WINDOWS\system32\cmd.exe /c ""D:\Program Files\Git\bin\sh.exe" --login -i -c "git commit -a -m '2013-05-06'""
+C:\WINDOWS\system32\cmd.exe /c ""D:\Program Files\Git\bin\sh.exe" --login -i -c "git commit -a -m %commit%""
 C:\WINDOWS\system32\cmd.exe /c ""D:\Program Files\Git\bin\sh.exe" --login -i -c "git push origin master""
 
 pause
