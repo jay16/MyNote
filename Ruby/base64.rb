6caedcf70c77e0279f533067fc0bdd9bafde17ab
jay16 <jay_li@intfocus.com>
2013-05-09 16:09:02 +0800

    2013-05-09_16:09:00.75

--- Ruby/base64.rb
+++ Ruby/base64.rb
@@ -1,7 +1,13 @@
 require 'base64'
 
+#º”√‹¬Î
 puts Base64.encode64('527130673')
 puts Base64.encode64('527130673890106')
+#Ω‚√‹
+puts Base64.decode64('NTI3MTMwNjcz')
+puts Base64.decode64('NTI3MTMwNjczODkwMTA2')
 
-NTI3MTMwNjcz
-NTI3MTMwNjczODkwMTA2
+p#uts Base64.decode_b('=?UTF-8?Q?Openfind=E7=94=B5=E5=AD=90=E6=8A=A5_2013/03?=')
+
+
+
