b2bd211faba6defd2cd70b0c36ec2a7ebe509018
jay16 <jay_li@intfocus.com>
2013-05-09 22:09:03 +0800

    2013-05-09_22:09:02.03

--- RubyGtk/SOLife/SOLife_Gtk.rb
+++ RubyGtk/SOLife/SOLife_Gtk.rb
@@ -9,13 +9,27 @@ current_dir = Dir.pwd
 SOLife_dir  = "E:\\MyWork\\MyNote\\"
 
 #根据SOLife_dir显示目录
-def generate_catalogue(note_dir)
-  if File.directory(note_dir) then
-    puts "enter:"+note_dir
-    Dir.foreach(note_dir) do |item|
-      puts item
+def generate_catalogue(root_dir,note_path,catalogue_array,parent_id)
+  if File.directory?(note_path) then
+    #puts "enter:"+note_path+"-basename:"+File.basename(note_path)+"-dirname:"+File.dirname(note_path);
+    if root_dir != note_path then
+    store_array.push({:file_type => "dir",:dirname => File.dirname(note_path), :basename => File.basename(note_path)})
+    
+    Dir.foreach(note_path) do |file|
+      if file != "." and file != ".." then
+        generate_catalogue(note_path+"\\"+file,store_array)
+      end
     end
-  else
+  elsif 
+    #puts "file:" + note_path+"-basename:"+File.basename(note_path)+"-dirname:"+File.dirname(note_path);
+    store_array.push(["file",:dirname => File.dirname(note_path), :basename => File.basename(note_path)})
   end
   
 end
+
+catalogue = Array.new
+generate_catalogue(SOLife_dir,catalogue)
+puts catalogue.to_s
+file = File.open("catalogue.txt","w")
+file.puts(catalogue.to_s)
+file.close
