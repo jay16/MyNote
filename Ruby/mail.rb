2cb4afe22346611f05e892ee5072033a00b3195b
jay16 <jay_li@intfocus.com>
2013-05-09 17:39:02 +0800

    2013-05-09_17:39:00.59

--- Ruby/mail.rb
+++ Ruby/mail.rb
@@ -14,4 +14,5 @@ puts "cc:     " + mail.cc.to_s         #=> 'sam@test.lindsaar.net'
 puts "subject:" + mail.subject         #=> "This is the subject"
 puts "date:   " + mail.date.to_s       #=> '21 Nov 1997 09:55:06 -0600'
 puts "msg_id: " + mail.message_id.to_s      #=> '<4D6AA7EB.6490534@xxx.xxx>'
+#如何跟踪mail的某个方法来看它的class或moudle，以便到githup看源代码
 #puts mail.body.decoded    #=> 'This is the body of the email...
