2ee223809eccce99fda4710cd4abdbe95ea5f79b
jay16 <jay_li@intfocus.com>
2013-05-10 22:39:02 +0800

    2013-05-10_22:39:00.53

--- RubyGtk/SOLife/Catalogue.rb
+++ RubyGtk/SOLife/Catalogue.rb
@@ -9,21 +9,21 @@ current_dir = Dir.pwd
 SOLife_dir  = "E:\\MyWork\\MyNote\\"
 
 #根据SOLife_dir显示目录
-def generate_catalogue(root_dir,note_path,catalogue_array,level_id)
-  level_id += 1
+def generate_catalogue(root_dir,note_path,catalogue_array,level)
+  level += 1
   if File.directory?(note_path) then
     #puts "enter:"+note_path+"-basename:"+File.basename(note_path)+"-dirname:"+File.dirname(note_path);
     if root_dir != note_path then
-      catalogue_array.push(["dir",note_path,File.basename(note_path),level_id])
+      catalogue_array.push(["dir",note_path,File.basename(note_path),level])
     end
     Dir.foreach(note_path) do |file|
       if file != "." and file != ".." then
-        generate_catalogue(root_dir,note_path+"\\"+file,catalogue_array,level_id)
+        generate_catalogue(root_dir,note_path+"\\"+file,catalogue_array,level)
       end
     end
   elsif 
     #puts "file:" + note_path+"-basename:"+File.basename(note_path)+"-dirname:"+File.dirname(note_path);
-    catalogue_array.push(["file",note_path,File.basename(note_path),level_id])
+    catalogue_array.push(["file",note_path,File.basename(note_path),level])
     
   end
 end
@@ -31,18 +31,21 @@ end
 catalogue = Array.new
 generate_catalogue(SOLife_dir,SOLife_dir,catalogue,0)
 
-catalogu_dir = catalogue.select { |item| item[0] == "dir" }
+level_one = catalogue.select { |item| item[-1] == 2 }
 
 file = File.open("catalogue.txt","w")
 
-catalogu_dir.each do |dir|
-  children = catalogue.select { |item| File.dirname(item[1]) == dir[1] }
+#为减少压力只显示两层
 
-  file.print(dir[2]+"-has " + children.count.to_s + "nodes-level_id:"+dir[-1].to_s)
-  file.print("\n");
-  children.each do |child|
-    file.print(dir[2]+" child:" + child[2]+" level_id:"+child[-1].to_s)
-    file.print("\n");
+level_one.each do |dir|
+  file.print("-"+dir[2])
+  file.print("\n")
+  puts "-"+dir[2]
+  level_two = catalogue.select { |item| File.dirname(item[1]) == dir[1] }
+  level_two.each do |item|
+    puts "  -"+item[2]
+    file.print("  -"+item[2])
+    file.print("\n")
   end
 end
 
--- RubyGtk/SOLife/SOLife_Gtk.rb
+++ RubyGtk/SOLife/SOLife_Gtk.rb
@@ -7,43 +7,81 @@ require 'gtk2'
 
 current_dir = Dir.pwd
 SOLife_dir  = "E:\\MyWork\\MyNote\\"
+SOLife_name = File.basename(SOLife_dir)
 
 #根据SOLife_dir显示目录
-def generate_catalogue(root_dir,note_path,catalogue_array)
+def generate_catalogue(root_dir,note_path,catalogue_array,level)
+  level += 1
   if File.directory?(note_path) then
     #puts "enter:"+note_path+"-basename:"+File.basename(note_path)+"-dirname:"+File.dirname(note_path);
     if root_dir != note_path then
-      catalogue_array.push(["dir",note_path,File.basename(note_path)])
+      catalogue_array.push(["dir",note_path,File.basename(note_path),level])
     end
     Dir.foreach(note_path) do |file|
       if file != "." and file != ".." then
-        generate_catalogue(root_dir,note_path+"\\"+file,catalogue_array)
+        generate_catalogue(root_dir,note_path+"\\"+file,catalogue_array,level)
       end
     end
   elsif 
     #puts "file:" + note_path+"-basename:"+File.basename(note_path)+"-dirname:"+File.dirname(note_path);
-    catalogue_array.push(["file",note_path,File.basename(note_path)])
+    catalogue_array.push(["file",note_path,File.basename(note_path),level])
     
   end
-  
 end
 
 catalogue = Array.new
-generate_catalogue(SOLife_dir,SOLife_dir,catalogue)
+generate_catalogue(SOLife_dir,SOLife_dir,catalogue,0)
 
-catalogu_dir = catalogue.select { |item| item[0] == "dir" }
+level_one = catalogue.select { |item| item[-1] == 2 }
 
-file = File.open("catalogue.txt","w")
+#为减少压力只显示两层
 
-catalogu_dir.each do |dir|
-  children = catalogue.select { |item| File.dirname(item[1]) == dir[1] }
+trees_tore = Gtk::TreeStore.new(String, String, Integer)
 
-  file.print(dir[2]+"-has " + children.count.to_s + "nodes")
-  file.print("\n");
-  children.each do |child|
-    file.print(dir[-1]+" child:" + child[-1])
-    file.print("\n");
+level_one.each do |lo|
+  tree_lo = trees_tore.append(nil)
+  tree_lo[0] = lo[2] #tree_level_one
+  level_two = catalogue.select { |item| File.dirname(item[1]) == lo[1] }
+  level_two.each do |lt|
+    tree_lt = trees_tore.append(tree_lo)
+    tree_lt[0] = lt[2]
   end
 end
 
-file.close
+
+
+tree_view = Gtk::TreeView.new(trees_tore)
+tree_view.selection.mode = Gtk::SELECTION_SINGLE
+tree_view.expand_all
+tree_view.hadjustment.value=100
+tree_view.columns_autosize
+#SELECTION_NONE
+#SELECTION_BROWSE
+scrolled_view = Gtk::ScrolledWindow.new
+scrolled_view.border_width = 2
+scrolled_view.add(tree_view)
+scrolled_view.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC)
+
+renderer = Gtk::CellRendererText.new
+col = Gtk::TreeViewColumn.new(SOLife_name, renderer, :text => 0)
+tree_view.append_column(col)
+
+
+table = Gtk::Table.new(1,    2,    true)
+
+
+options = Gtk::EXPAND|Gtk::FILL
+            # child, x1, x2, y1, y2, x-opt,   y-opt,   xpad, ypad
+table.attach(label1,  0,  1,  0,  1, options, options, 0,    0)
+table.attach(label2,  0,  1,  1,  2, options, options, 0,    0)
+table.attach(name,    ,  2,  1,  2, options, options, 0,    0)
+
+
+window = Gtk::Window.new("")
+
+window.signal_connect("destroy") { Gtk.main_quit }
+hbox = Gtk::HBox.new(homogeneous=false, spacing=0) 
+hbox.pack_start(scrolled_view,true,true,0)
+window.add(hbox)
+window.show_all
+Gtk.main
