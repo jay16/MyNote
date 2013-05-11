f6807607eb92619c8e24de5ea3761e8ccf43ccbf
jay16 <jay_li@intfocus.com>
2013-05-11 10:09:06 +0800

    2013-05-11_10:09:00.67

--- RubyGtk/SOLife/SOLife_Action.rb
+++ RubyGtk/SOLife/SOLife_Action.rb
@@ -1,262 +1,33 @@
 
-def add_theme(treeview,note_save)
-  # Create a dialog that will be used to create a new product.
 
-  dialog = Gtk::Dialog.new(
-      "Add a Theme",
-      nil,
-      Gtk::Dialog::MODAL,
-      [ Gtk::Stock::ADD,    Gtk::Dialog::RESPONSE_OK ],
-      [ Gtk::Stock::CANCEL, Gtk::Dialog::RESPONSE_CANCEL ]
-  )
-  entry = Gtk::Entry.new
-
-  spin  = Gtk::SpinButton.new(0, 100, 1)
-  # Set the precision to be displayed by spin button.
-  spin.digits = 0
-
-  table = Gtk::Table.new(4, 2, false)
-  table.row_spacings = 5
-  table.column_spacings = 5
-  table.border_width = 5
-
-  # Pack the table that will hold the dialog widgets.
-  fll_shr = Gtk::SHRINK | Gtk::FILL
-  fll_exp = Gtk::EXPAND | Gtk::FILL
-
-
-  table.attach(Gtk::Label.new("Theme:"),  0, 1, 1, 2, fll_shr, fll_shr,  0, 0)
-  table.attach(entry,                       1, 2, 1, 2, fll_exp, fll_shr,  0, 0)
-
-  dialog.vbox.pack_start_defaults(table)
-  dialog.show_all
-
-  dialog.run do |response|
-    if response == Gtk::Dialog::RESPONSE_OK
-      theme = entry.text
-
-      if theme == ""
-        dialog.destroy
-        return
-      end
-
-      model = treeview.model
-
-      parent = model.append(nil)
-      parent[0]  = theme
-      note_save.transaction do 
-        note_save[theme] = nil
-      end
+def row_activated(tree_view,text_view,window,note_label)
+ selection = tree_view.selection
+ if iter = selection.selected    
+    node_name = iter[0] 
+    node_path = iter[1]
+    if File.file? node_path
+      note_label.text = node_name
+      text_view.buffer.text = File.readlines(node_path).join("").to_s
+      text_view.buffer.text = "hello no chinese"
+      #puts File.readlines(node_path).join('\n')
+    else
+      puts "dir"
     end
-    dialog.destroy
-  end
-end
 
-def add_segment(treeview,treestore,note_save)
-  # Create a dialog that will be used to create a new product.
-
-  dialog = Gtk::Dialog.new(
-      "Add a Segment",
-      nil,
-      Gtk::Dialog::MODAL,
-      [ Gtk::Stock::ADD,    Gtk::Dialog::RESPONSE_OK ],
-      [ Gtk::Stock::CANCEL, Gtk::Dialog::RESPONSE_CANCEL ]
-  )
-  # Create widgets that will be packed into the dialog.
-
-  entry = Gtk::Entry.new
-
-  table = Gtk::Table.new(4, 2, false)
-  table.row_spacings = 5
-  table.column_spacings = 5
-  table.border_width = 5
-
-    model = treeview.model
-    selection = treeview.selection.selected
-    parent = selection.parent 
-    parent = selection unless parent
-    
-    row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(parent.to_s)) if parent
-    row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(selection.to_s)) unless parent
-    theme = row_ref.model.get_iter(row_ref.path)[0]
-    
-  # Pack the table that will hold the dialog widgets.
-  fll_shr = Gtk::SHRINK | Gtk::FILL
-  fll_exp = Gtk::EXPAND | Gtk::FILL
-  table.attach(Gtk::Label.new("Root:#{theme}"), 0, 1, 0, 1, fll_shr, fll_shr,  0, 0)
-  table.attach(Gtk::Label.new("Segment:"),  0, 1, 1, 2, fll_shr, fll_shr,  0, 0)
-  table.attach(entry,                       1, 2, 1, 2, fll_exp, fll_shr,  0, 0)
-
-
-    
-  dialog.vbox.pack_start_defaults(table)
-  #dialog.set_title(theme)
-  dialog.show_all
-
-  dialog.run do |response|
-    # If the user presses OK, verify the entries and add the product.
-    if response == Gtk::Dialog::RESPONSE_OK
-      segment = entry.text
-      if segment == ""
-        dialog.destroy
-        return
-      end
-
-   
-      iter = model.get_iter(parent.to_s)
-
-      child = model.append(iter)
-      child[0]   = segment
-      note_save.transaction do 
-        note_save[theme] = { segment => "no data"}
-      end
-    end
-    dialog.destroy
-  end
-end
-
-def add_node(treeview,treestore,note_save,note_load,cur_dir)
-  dialog = Gtk::Dialog.new(
-      "Add a Node",
-      nil,
-      Gtk::Dialog::MODAL,
-      [ Gtk::Stock::ADD,    Gtk::Dialog::RESPONSE_OK ],
-      [ Gtk::Stock::CANCEL, Gtk::Dialog::RESPONSE_CANCEL ]
-  )
-  # Create widgets that will be packed into the dialog.
-
-  entry = Gtk::Entry.new
-
-  table = Gtk::Table.new(4, 2, false)
-  table.row_spacings = 5
-  table.column_spacings = 5
-  table.border_width = 5
-
-  model = treeview.model
-  selection = treeview.selection.selected
-  str_path = Array.new
-  if selection
-    row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(selection.to_s))
-    node = row_ref.model.get_iter(row_ref.path)[0]
-    name_path = row_ref.path.to_str.split(":")
-    temp_path = row_ref.path.to_str.split(":")
-
-
-    name_path.each do |item|
-      row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(temp_path.join(":").to_s))
-      str_path.push(row_ref.model.get_iter(row_ref.path)[0])
-      temp_path.delete_at(temp_path.length-1)
-    end
-    str_path.reverse!
-  else
-  end
-
-  
-  # Pack the table that will hold the dialog widgets.
-  fll_shr = Gtk::SHRINK | Gtk::FILL
-  fll_exp = Gtk::EXPAND | Gtk::FILL
-  table.attach(Gtk::Label.new("parent:#{node}"), 0, 1, 0, 1, fll_shr, fll_shr,  0, 0)
-  table.attach(Gtk::Label.new("node:"),  0, 1, 1, 2, fll_shr, fll_shr,  0, 0)
-  table.attach(entry,                       1, 2, 1, 2, fll_exp, fll_shr,  0, 0)
-
-
-    
-  dialog.vbox.pack_start_defaults(table)
-  dialog.show_all
-
-  dialog.run do |response|
-    # If the user presses OK, verify the entries and add the product.
-    if response == Gtk::Dialog::RESPONSE_OK
-      child_name = entry.text
-   
-      iter = model.get_iter(selection.to_s) if selection
-      iter = nil unless selection
-      child = model.append(iter)
-      child[0]   = child_name
-      note_save.transaction do
-        case str_path.length
-        when 0 then
-          parent = model.append(nil)
-          parent[0]  = child_name
-          note_save.transaction do 
-            note_save[child_name] = nil
-          end
-        when 1 then 
-          parchild = model.append(child)
-          parchild[0] = "content"
-          note_save[str_path[0]].merge!({ child_name => {"content"=>"no data"}}) if note_save[str_path[0]]
-          note_save[str_path[0]] = { child_name => {"content"=>"no data"}} unless note_save[str_path[0]]
-          dir_path = File.join(cur_dir,"note",str_path[0],child_name)
-          unless File.exist?(dir_path)
-            Dir.mkdir(dir_path)
-            yml_file = File.join(dir_path,"content.yml")
-            db = YAML::Store.new(yml_file)
-            db.transaction do
-              db["content"] = "[#{str_path[0]}][#{child_name}][content] no data"
-            end
-          end
-        when 2 then
-          note_save[str_path[0]][str_path[1]].merge!({ child_name => "SoLife Param!"})
-          yml_file = File.join(cur_dir,"note",str_path[0],str_path[1],"#{child_name}.yml")
-          unless File.exist?(yml_file)
-           db = YAML::Store.new(yml_file)
-           db.transaction do
-             db["content"] = "[#{str_path[0]}][#{str_path[1]}][#{child_name}] no data"
-           end
-          end
-        else
-        end
-      end
-    end
-    dialog.destroy
-  end
-end
-
-def row_activated(treeview,treestore,textview,window,cur_dir)
- selection = treeview.selection
- if iter = selection.selected        
-   segment = iter[0]
-   #记录当前被选择行的路径
-   #selection.selected_each do |model, path, iter|
-   #  puts path
-   #end
-  
-   row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(iter.to_s))
-   name_path = row_ref.path.to_str.split(":")
-   if name_path.length==3
-     temp_path = row_ref.path.to_str.split(":")
-     str_path = Array.new
-     name_path.each do |item|
-       row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(temp_path.join(":").to_s))
-       str_path.push(row_ref.model.get_iter(row_ref.path)[0])
-       temp_path.delete_at(temp_path.length-1)
-     end
-     str_path.reverse!
-     
-     yml_file = File.join(cur_dir,"note",str_path[0],str_path[1],"#{str_path[2]}.yml")
-     unless File.exist?(yml_file)
-      outfile=File.new(yml_file,"w")
-      outfile.close
-     end
-    window.set_title("SoLife [#{str_path[0]}][#{str_path[1]}][#{str_path[2]}] [open]")
-    window.show_all
-   
-    textview.buffer.text = YAML.load_file(yml_file)["content"]
-   end
- else
  end
+   
 end
 
-def save_file(treeview,treestore,textview,window,cur_dir) 
- selection = treeview.selection
+def save_file(tree_view,tree_store,text_view,window,cur_dir) 
+ selection = tree_view.selection
  if iter = selection.selected        
-   row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(iter.to_s))
+   row_ref = Gtk::TreeRowReference.new(tree_store, Gtk::TreePath.new(iter.to_s))
    name_path = row_ref.path.to_str.split(":")
    if name_path.length==3
      temp_path = row_ref.path.to_str.split(":")
      str_path = Array.new
      name_path.each do |item|
-       row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(temp_path.join(":").to_s))
+       row_ref = Gtk::TreeRowReference.new(tree_store, Gtk::TreePath.new(temp_path.join(":").to_s))
        str_path.push(row_ref.model.get_iter(row_ref.path)[0])
        temp_path.delete_at(temp_path.length-1)
      end
@@ -264,7 +35,7 @@ def save_file(treeview,treestore,textview,window,cur_dir)
      yml_file = File.join(cur_dir,"note",str_path[0],str_path[1],"#{str_path[2]}.yml")
      db = YAML::Store.new(yml_file)
      db.transaction do
-       db["content"] = textview.buffer.text
+       db["content"] = text_view.buffer.text
      end
      window.set_title("SoLife [#{str_path[0]}][#{str_path[1]}][#{str_path[2]}] [saved]")
      window.show_all
@@ -275,15 +46,15 @@ def save_file(treeview,treestore,textview,window,cur_dir)
  end
 end
 
-def write_statu(treeview,treestore,textview,window) 
- selection = treeview.selection
+def write_statu(tree_view,tree_store,text_view,window) 
+ selection = tree_view.selection
  if iter = selection.selected        
-   row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(iter.to_s))
+   row_ref = Gtk::TreeRowReference.new(tree_store, Gtk::TreePath.new(iter.to_s))
    name_path = row_ref.path.to_str.split(":")
    temp_path = row_ref.path.to_str.split(":")
    str_path = Array.new
    name_path.each do |item|
-     row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(temp_path.join(":").to_s))
+     row_ref = Gtk::TreeRowReference.new(tree_store, Gtk::TreePath.new(temp_path.join(":").to_s))
      str_path.push(row_ref.model.get_iter(row_ref.path)[0])
      temp_path.delete_at(temp_path.length-1)
    end
@@ -296,15 +67,15 @@ def write_statu(treeview,treestore,textview,window)
  end
 end
 
-def window_exit(treeview,treestore,note_record,window)
- selection = treeview.selection
+def window_exit(tree_view,tree_store,note_record,window)
+ selection = tree_view.selection
  if iter = selection.selected        
-   row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(iter.to_s))
+   row_ref = Gtk::TreeRowReference.new(tree_store, Gtk::TreePath.new(iter.to_s))
    name_path = row_ref.path.to_str.split(":")   
    temp_path = row_ref.path.to_str.split(":")
    str_path = Array.new
    name_path.each do |item|
-     row_ref = Gtk::TreeRowReference.new(treestore, Gtk::TreePath.new(temp_path.join(":").to_s))
+     row_ref = Gtk::TreeRowReference.new(tree_store, Gtk::TreePath.new(temp_path.join(":").to_s))
      str_path.push(row_ref.model.get_iter(row_ref.path)[0])
      temp_path.delete_at(temp_path.length-1)
    end
@@ -320,7 +91,7 @@ def window_exit(treeview,treestore,note_record,window)
  end
  Gtk.main_quit
 end
-# Search for the entered string within the GtkTextView.
+# Search for the entered string within the Gtktext_view.
 # Then tell the user how many times it was found.
 def search(ent, txtvu)
   start = txtvu.buffer.start_iter
--- RubyGtk/SOLife/SOLife_Gtk.rb
+++ RubyGtk/SOLife/SOLife_Gtk.rb
@@ -2,6 +2,7 @@
 require 'yaml/store'
 require 'yaml'
 require 'gtk2'
+require './SOLife_Action.rb'
 
 
 
@@ -15,7 +16,7 @@ def generate_catalogue(root_dir,note_path,catalogue_array,level)
   if File.directory?(note_path) then
     #puts "enter:"+note_path+"-basename:"+File.basename(note_path)+"-dirname:"+File.dirname(note_path);
     if root_dir != note_path then
-      catalogue_array.push(["dir",note_path,File.basename(note_path),level])
+      catalogue_array.push([File.basename(note_path),note_path,"dir",level])
     end
     Dir.foreach(note_path) do |file|
       if file != "." and file != ".." then
@@ -24,33 +25,38 @@ def generate_catalogue(root_dir,note_path,catalogue_array,level)
     end
   elsif 
     #puts "file:" + note_path+"-basename:"+File.basename(note_path)+"-dirname:"+File.dirname(note_path);
-    catalogue_array.push(["file",note_path,File.basename(note_path),level])
+    catalogue_array.push([File.basename(note_path),note_path,"file",level])
     
   end
 end
 
 catalogue = Array.new
-generate_catalogue(SOLife_dir,SOLife_dir,catalogue,0)
+generate_catalogue(SOLife_dir,SOLife_dir,catalogue,-1)
 
-level_one = catalogue.select { |item| item[-1] == 2 }
+level_one = catalogue.select { |item| item[-1] == 1 }
+
 
 #为减少压力只显示两层
-
-trees_tore = Gtk::TreeStore.new(String, String, Integer)
+tree_store = Gtk::TreeStore.new(String, String, Integer)
 
 level_one.each do |lo|
-  tree_lo = trees_tore.append(nil)
-  tree_lo[0] = lo[2] #tree_level_one
+  #level one
+  tree_lo = tree_store.append(nil)
+  tree_lo[0] = lo[0] #tree_level_one nodename
+  tree_lo[1] = lo[1] #tree_level_one nodepath
+  #tree_lo[2] = lo[2] #tree_level_one nodetype
   level_two = catalogue.select { |item| File.dirname(item[1]) == lo[1] }
   level_two.each do |lt|
-    tree_lt = trees_tore.append(tree_lo)
-    tree_lt[0] = lt[2]
+    tree_lt = tree_store.append(tree_lo)
+    tree_lt[0] = lt[0]
+    tree_lt[1] = lt[1]
+    #tree_lt[2] = lt[2]
   end
 end
 
 
 
-tree_view = Gtk::TreeView.new(trees_tore)
+tree_view = Gtk::TreeView.new(tree_store)
 tree_view.selection.mode = Gtk::SELECTION_SINGLE
 #tree_view.expand_all
 tree_view.hadjustment.value=100
@@ -62,20 +68,25 @@ scrolled_view.border_width = 2
 scrolled_view.add(tree_view)
 scrolled_view.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC)
 
-#树形结点
+#目录框架
 renderer = Gtk::CellRendererText.new
-col = Gtk::TreeViewColumn.new(SOLife_name, renderer, :text => 0)
-tree_view.append_column(col)
+tree_col = Gtk::TreeViewColumn.new(SOLife_name, renderer, :text => 0)
+tree_view.append_column(tree_col)
 
 #右侧水平容器
 left_hbox = Gtk::HBox.new(homogeneous=false, spacing=0) 
 left_hbox.pack_start(scrolled_view,true,true,0)
 
-#记事本
+#记事本框架
 note_book = Gtk::Notebook.new
-note1  = Gtk::Label.new("notebooxk")
-note_book.append_page(note1, note1)
-
+note_label  = Gtk::Label.new("notebooxk")
+#记事本内容
+text_view = Gtk::TextView.new
+text_view.buffer.text = "Your 1st Gtk::TextView widget!"
+text_font = Pango::FontDescription.new("Monospace Normal 10")
+text_view.modify_font(text_font)
+#记事本填充
+note_book.append_page(text_view,note_label)
 
 #整体布局表格
 table = Gtk::Table.new(1, 4,true)
@@ -91,7 +102,10 @@ table.attach(note_book,  1,  4,  0,  1, options, options, 0,    0)
 
 window = Gtk::Window.new("")
 
+#点击关闭
 window.signal_connect("destroy") { Gtk.main_quit }
+#目录文件被点击时，使用记事本打开
+tree_view.signal_connect("row-activated") { row_activated(tree_view,text_view,window,note_label)}
 
 window.add(table)
 window.set_title("SoLife")
