95d38e9e6e3aec419cef95aa9a7154e53443170d
jay16 <jay_li@intfocus.com>
2013-05-20 17:39:05 +0800

    2013-05-20_17:39:01.20

--- Ruby/rexml.rb
+++ Ruby/rexml.rb
@@ -1,20 +1,16 @@
-    require 'rexml/document'  
-    require 'rexml/streamlistener'  
-    include REXML  
-      
-    class MyListener  
-      include REXML::StreamListener  
-      def tag_start(*args)  
-        puts "tag_start: #{args.map {|x| x.inspect}.join(', ')}"  
-      end  
-      
-      def text(data)  
-        return if data =~ /^\w*$/     # whitespace only  
-        abbrev = data[0..40] + (data.length > 40 ? "..." : "")  
-        puts "  text   :   #{abbrev.inspect}"  
-      end  
-    end  
-      
-    list = MyListener.new  
-    source = File.new "books.xml"  
-    Document.parse_stream(source, list)  
+require "rexml/document"
+include REXML  # so that we don't have to prefix everything with REXML::...
+
+source = "<a xmlns:x='foo' xmlns:y='bar'><h>helloworld</h><x:b id='1'/><y:b id='2'/></a>"
+doc = Document.new source
+puts doc.elements["/a/x:b"].attributes["id"]	                     # -> '1'
+puts XPath.first(doc, "/a/m:b", {"m"=>"bar"}).attributes["id"]   # -> '2'
+puts doc.elements["//x:b"].prefix                                # -> 'x'
+puts doc.elements["//x:b"].namespace	                             # -> 'foo'
+puts XPath.first(doc, "//m:b", {"m"=>"bar"}).prefix              # -> 'y'
+puts doc.elements["/a/h"].text                   # -> '1'
+
+
+file = File.new("kettle.xml")
+doc = Document.new file
+puts doc.elements["/transformation/connection/name"].text
